94e44dd84c12f78162c441431b06eadaf377e0a0
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-02-08 19:28:56 +0100

    Add files via upload

--- src/Componenti/home.js
+++ src/Componenti/home.js
@@ -4,7 +4,7 @@ import { Story } from "./API/story";
 import LoadMore from "./loadMore";
 
 export const Home = () => {
-    const [ids, setIds] = useState({});
+    const [ids, setIds] = useState([]);
     const [number, setNumber] = useState(10);
 
     function mostraAltro() {setNumber(number + 10);}
@@ -15,7 +15,7 @@ export const Home = () => {
     
     return (
       <div>
-        <div className="">
+        <div>
           { ids.slice(0, number).map(id => <Story key={id} typeOfStory={id} />) }
         </div>
       <LoadMore onClick={mostraAltro} />
