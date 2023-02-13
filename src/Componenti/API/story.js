7f0b3f53f02bdc78c13902005262616ce0903d44
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-02-13 17:49:35 +0100

    Update story.js

--- src/Componenti/API/story.js
+++ src/Componenti/API/story.js
@@ -12,11 +12,10 @@ export const Story = ({typeOfStory}) => {
      }, [])
 
      return (
-        story.url ?
-        (<div className='border-black border-2 m-5 text-xs md:text-base bg-[#111d5e] text-white'>
+        <div className='border-black border-2 m-5 text-xs md:text-base bg-[#111d5e] text-white'>
         <h3 className='text-lg hover:text-xl md:text-xl md:hover:text-2xl'><a href={story.url}>{story.title}</a></h3>
         <h4>Author: {story.by}</h4>
         <h4>Date: {theDate(story.time)}</h4>
-        </div>) : undefined
+        </div>
       );
 }
